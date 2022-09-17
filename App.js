e596c980d10600958e5d1faea17a78e8e711f09c
TommiVLavi <bgr912@myy.haaga-helia.fi>
2022-09-17 19:07:52 +0300

    Teht 6 suoritettu

--- App.js
+++ App.js
@@ -1,10 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, FlatList, Alert, Image } from 'react-native';
 
 export default function App() {
+
+  const [keyword, setKeyword] = useState('');
+  const [repositories, setRepositories] = useState([])
+
+  const getRepositories = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}` )
+    .then(response => response.json())
+    .then(data => setRepositories(data.meals))
+    .catch(error => {
+      Alert.alert('Jokin meni pieleen', error)
+    })
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <FlatList
+        style={{width:350}}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => 
+          <View>
+            <Text style={{fontSize: 17, fontWeight: "bold"}}> {item.strMeal} </Text>
+          
+            <Image 
+              style={{ width: 100, height: 100}}
+              source={{ uri: `${item.strMealThumb}`}}
+            />
+            
+          </View>
+        }
+        data={repositories}
+      />
+
+      <TextInput style={styles.input}
+        placeholder='Antaisitko hakusanan, kiitos.'
+        onChangeText={ text => setKeyword(text) }        
+      />
+
+      <Button
+        title='Etsi'
+        onPress={getRepositories}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +59,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    fontSize: 23,
+    width: 300
+  }
 });
